d7be8c186afba4e4e25b3c61ee0c34a8b1e9a842
LMatysiak777 <lukmat7@gmail.com>
2022-02-23 10:59:36 +0100

    added extension methods

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,11 +5,15 @@ using API.Entities;
 using System.Linq;
 using System.Threading.Tasks; 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    // [ApiController]
+    // [Route("api/[controller]")]
+    
+    // every controller inherits from controllerbase:
+    // ApiController and Route properties now inherited from BaseApiController class:
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -17,14 +21,16 @@ namespace API.Controllers
             _context = context;
         }
 
+        
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = _context.Users.ToListAsync();
             return await users; 
         }
-        
-         [HttpGet("{id}")]
+        [Authorize]
+        [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,12 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
 using API.Controllers;
+using API.interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using API.Extensions;
+using System.Text;
 
 namespace API
 {
@@ -30,13 +36,13 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => 
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+
+            services.AddApplicationServices(_config);
             services.AddControllers();
             // adding CORS policy:
             services.AddCors();
+            services.AddIdentityServices(_config);
+        
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -59,7 +65,7 @@ namespace API
 
             // specific place to invoke UseCors
             app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-            
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

97de08d420d85a70f5712b993e023dd796dca4f2
LMatysiak777 <lukmat7@gmail.com>
2022-02-24 10:22:33 +0100

    updated child parent communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,9 +11,10 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit{
   title = 'The Dating App';
   users : any;
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  // constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit()  {
-    this.getUsers();
+    // this.getUsers();
     this.setCurrentUser();
     // throw new Error('Method not implemented');
   }
@@ -23,12 +24,12 @@ export class AppComponent implements OnInit{
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers() {
+  // getUsers() {
     
-    this.http.get("https://localhost:5001/api/users").subscribe(response=>{
-      this.users=response;
-    }, error => {
-      console.log(error);
-    });
-  }
+  //   this.http.get("https://localhost:5001/api/users").subscribe(response=>{
+  //     this.users=response;
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }
 }
--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+ 
 
 @Component({
   selector: 'app-homepage',
@@ -6,16 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  registerMode = false ; 
+  registerMode = false; 
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  //example of initializing method to fetch user DTO from API
+    // this.getUsers()
   }
 
   // flag for toggling DOM generation of certain sections on hompage component
   registerToggle() {
     this.registerMode =!this.registerMode;
+  }
+  // example of method for fetching users DTO from API 
+  // getUsers() {
+  //   this.http.get("https://localhost:5001/api/users/").subscribe(users=> this.users = users)
+  // }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event; 
+    
   }
 
 }
